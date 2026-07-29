9f2643e8ac83b964e4d46710d598cd005fe7defe
Nohkeyra <122115637+Nohkeyra@users.noreply.github.com>
2026-07-30 06:11:50 +0800

    Up

--- server.ts
+++ server.ts
@@ -50,7 +50,7 @@ import {
 async function startServer() {
   const app = express();
   app.set("trust proxy", 1);
-  const PORT = process.env.PORT ? parseInt(process.env.PORT) : 3000;
+  const PORT = 3000;
 
   // Middleware
   const DEFAULT_ORIGINS = [
