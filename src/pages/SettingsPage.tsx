cb227a062f415598972000d6107579ff54a70a55
Nohkeyra <122115637+Nohkeyra@users.noreply.github.com>
2026-08-06 15:20:39 +0800

    build: update app download link to v7.0 Wawasan.Hub

--- src/pages/SettingsPage.tsx
+++ src/pages/SettingsPage.tsx
@@ -275,8 +275,8 @@ export default function SettingsPage() {
 
                 <div className="flex flex-col sm:flex-row gap-3 pt-2">
                   <a
-                    href="https://github.com/Nohkeyra/Pu/releases/download/Wawasan/Restoran-Wawasan.apk"
-                    download="Restoran-Wawasan.apk"
+                    href="https://github.com/Nohkeyra/Pu/releases/download/v7.0/Wawasan.Hub.apk"
+                    download="Wawasan.Hub.apk"
                     target="_blank"
                     rel="noopener noreferrer"
                     onClick={async () => {
